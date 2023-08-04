7307084f861a86df75039b0060b6f31fca4d150e
Lamis Armoush <lamis.armoush@hotmail.com>
2023-08-04 03:11:01 +0300

    final edits

--- Kata08/src/Main.java
+++ Kata08/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//        Attempt1Readable a1 = new Attempt1Readable("C:\Users\user\Desktop\Kudwa_Assessment_LamisArmoush\Kata08\src\wordlist.txt");
+//        Attempt1Readable a1 = new Attempt1Readable("src\wordlist.txt");
 //        a1.process_dictionary();
-//        Attempt2Faster a2 = new Attempt2Faster("C:\Users\user\Desktop\Kudwa_Assessment_LamisArmoush\Kata08\src\wordlist.txt");
+//        Attempt2Faster a2 = new Attempt2Faster("src\wordlist.txt");
 //        a2.process_dictionary();
-//        Attempt3Extendible a3 = new Attempt3Extendible("C:\Users\user\Desktop\Kudwa_Assessment_LamisArmoush\Kata08\src\wordlist.txt",6);
-//        a3.process_dictionary();
-            Attempt4AllCombinations a4 = new Attempt4AllCombinations();
-            a4.process("C:\\Users\\user\\Desktop\\Kudwa_Assessment_LamisArmoush\\Kata08\\src\\wordlist.txt",2,30);
+        Attempt3Extendible a3 = new Attempt3Extendible("C:\\Users\\user\\Desktop\\Kudwa_Assessment_LamisArmoush\\Kata08\\src\\wordlist.txt",6);
+        a3.process_dictionary();
+//            Attempt4AllCombinations a4 = new Attempt4AllCombinations();
+//            a4.process("C:\\Users\\user\\Desktop\\Kudwa_Assessment_LamisArmoush\\Kata08\\src\\wordlist.txt",2,30);
 
     }
 }
